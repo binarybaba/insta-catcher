4c1d0eab223b55ae55f5358ecf2d7487d8fc2140
binarybaba <amin.ajani17@gmail.com>
2021-10-11 11:34:58 +0200

    add prettier

--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,15 @@
-import express from 'express';
-import { PORT } from "./config";
+import express from 'express'
+import { PORT } from './config'
 
-
-const app = express();
+const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
 app.post('/catch', (req, res) => {
-    res.send('')
+  res.send('')
 })
 
 app.listen(PORT, () => {
-    console.log('Listening on', PORT)
+  console.log('Listening on', PORT)
 })

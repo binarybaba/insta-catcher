9b95d7e063e50bc0176695906471184997bcbe4a
binarybaba <amin.ajani17@gmail.com>
2021-10-11 14:34:01 +0200

    Add state types

---
+++ client/src/state/types.ts
@@ -0,0 +1,22 @@
+import { Dispatch } from 'react'
+
+export type State = {
+  isRequesting: boolean
+  mediaLink: string | null
+  instagramLink: string | null
+  isMediaVideo: boolean | null
+}
+
+export enum ActionType {
+  SET_IS_REQUESTING = 'SET_IS_REQUESTING',
+  SET_MEDIA_LINK = 'SET_MEDIA_LINK',
+}
+
+export type Action =
+  | { type: ActionType.SET_IS_REQUESTING; isRequesting: boolean; instagramLink: string }
+  | { type: ActionType.SET_MEDIA_LINK; mediaLink: string; isMediaVideo: boolean }
+
+export type Context = {
+  state: State
+  dispatch: Dispatch<any>
+}

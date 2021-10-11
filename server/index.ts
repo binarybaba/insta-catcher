92d4e9a5a4daf039189c2d157766dcf5748ba2fa
binarybaba <amin.ajani17@gmail.com>
2021-10-11 11:32:13 +0200

    move server to index and add nodemon

---
+++ server/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import { PORT } from "./config";
+
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.post('/catch', (req, res) => {
+    res.send('')
+})
+
+app.listen(PORT, () => {
+    console.log('Listening on', PORT)
+})

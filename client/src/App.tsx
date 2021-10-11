5ac9424af8065b44044f5969ca4629df0dd9b5bb
binarybaba <amin.ajani17@gmail.com>
2021-10-11 15:43:19 +0200

    Clean up CRA bloatware
    The makeup will begin shortly!
    Been banging my head to make craco boilerplate and tailwind work since the past 2 days now.
    Should have simply done this.
    I feel I'm a special kind of idiot.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,13 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+
 import Interaction from 'src/components/interaction/Interaction'
-import Thumbnail from "./components/thumbnail/Thumbnail";
+import Thumbnail from "src/components/thumbnail/Thumbnail";
 import AppProvider from 'src/state/app/Provider'
 function App() {
   return (
     <AppProvider>
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-            Learn React
-          </a>
-        </header>
-        <Thumbnail />
-        <Interaction />
-      </div>
+      <Thumbnail />
+      <Interaction />
     </AppProvider>
   )
 }
